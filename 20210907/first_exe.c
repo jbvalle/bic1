e11bb9d199b86959b84b8b26d2888d3c55bf35e5
John Bryan Valle <jbvalle.jv@gmail.com>
2021-09-07 20:16:08 +0200

    added variables

--- 20210907/first_exe.c
+++ 20210907/first_exe.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
+
+  float a = 2.1;
   printf("John Valle\n");
   printf("2110258005\n");
   printf("A\n");
+  printf("%f\n", a);
   return 0;
 }
